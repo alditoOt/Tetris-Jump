a94549c759710ed91ad2686f50a970e4ad7c92e6
Aldo Oporto <a_r_oporto@outlook.com>
2021-06-26 15:42:58 -0400

    added main menu items and transition

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject screenTransition;
+    public void StartScreenTransition()
+    {
+        screenTransition.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
